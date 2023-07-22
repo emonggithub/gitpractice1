b0f0f3bd409bf8e2e9906bb2fcbc62f9a38bb8fd
emonggithub <paul.emong@gmail.com>
2023-07-22 11:19:35 +0300

    changes made on file1.py

--- file1.py
+++ file1.py
@@ -1,7 +1,8 @@
-class Person:
-  def __init__(self,name,age):
-    self.nam1 = name
+class Person: # Definig Person class
+  def __init__(self,name,age): # Defining the constructor
+    self.name1 = name # Instance variables
     self.age1 = age
     print(f"{self.name1}: {self.age1}")
 
+# Creating the instance/object called person1 of Person class
 person1 = Person("Paul",37)
